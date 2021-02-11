4d1efd1263551f632c9f8b5066a183bb835d1c65
dantesnake <jonas50_34@hotmail.com>
2021-02-11 18:58:55 +0000

    nuevo

---
+++ c++/secc1/helloworld.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+	int main() {
+	    count << "!!!Hello there world!!!" <<< end1; //imprime hola mundo
+	    return 0;
+
+}
---
+++ c++/secc2/strings.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+ string text1 = "Hello ";
+ string text2 = "Jona ";
+ string text3 = text1 + text2; 
+ 
+ cout << text1 << text2 << endl;
+
+
+ return 0;
+
+}
